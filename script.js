3ec4dabe2378212263f5a1f815f74feb0154e6a9
bagusJatiPramono <bjpramono@student.ub.ac.id>
2026-01-08 13:39:00 +0700

    adding script to website (sliding wall)

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const button = document.querySelector('.menu-btn');
-const nav = document.querySelector('.nav');
+const nav = document.querySelector('.side-nav');
 
 button.addEventListener('click',() => {
     nav.classList.toggle('active');
